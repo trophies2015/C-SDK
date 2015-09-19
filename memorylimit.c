04ebd628038a25d63319710ec1cdd049b1c4c756
trophies2015 <patelshwetark@gmail.com>
2015-09-19 09:39:17 -0400

    tell me what this does on your machine

--- memorylimit.c
+++ memorylimit.c
@@ -1,15 +1,19 @@
 #include <sys/resource.h> 
- #include <sys/time.h> 
- #include <unistd.h> 
- #include<stdio.h> 
-  
- int main () 
+#include <sys/time.h> 
+#include <unistd.h> 
+#include<stdio.h> 
+#include <stdlib.h>  
+int main () 
  { 
    struct rlimit rl;   
-   rl.rlim_cur = 1; 
-   rl.rlim_max = 1; 
-   setrlimit (RLIMIT_CPU, &rl); 
-   int a = 0;
-   while (1) a++;
+   rl.rlim_cur = 100; 
+   rl.rlim_max = 100; 
+   setrlimit (RLIMIT_AS, &rl); 
+   getrlimit (RLIMIT_AS, &rl); 
+   printf("%lld\n", rl.rlim_cur);
+   char * p; 
+   p = (char *) malloc (1024);
+   if (p == NULL) printf("%s\n", "FAILED"); 
+   else printf("%s\n", "SUCCESS"); 
    return 0; 
  }
